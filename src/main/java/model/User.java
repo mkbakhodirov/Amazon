5f2690bfee5f9bf377d2dc72f69afe1beb67ad3e
Muzaffar <bakhodirov.muzaffar2001@gmail.com>
2021-12-08 18:06:10 +0500

    Start

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,15 @@
 package model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import model.base.BaseModel;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseModel {
+
 }
--- src/main/java/model/base/BaseModel.java
+++ src/main/java/model/base/BaseModel.java
@@ -1,4 +1,18 @@
 package model.base;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class BaseModel {
+    private final UUID id;
+
+    {
+        id = UUID.randomUUID();
+    }
 }
