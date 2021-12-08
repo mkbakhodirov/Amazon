add6ed50da10b6815b6921aaf933b1e230732e7a
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2021-12-08 17:37:43 +0500

    solavat

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Muzaffar nima deb yozay a??? ....." +
+                "Allohumma solli wa sallim ala Muhammad wa ala alihi wa ashabihi ajmain");
     }
 }
