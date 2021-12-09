560b5dd7c795bba091ea3a5e94596fe26a56d7e1
Muzaffar <bakhodirov.muzaffar2001@gmail.com>
2021-12-09 17:01:02 +0500

    History & UI

--- src/main/java/model/Amount.java
+++ src/main/java/model/Amount.java
@@ -5,12 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Amount {
-    Double discount;
-    Double balance;
-    UUID userId;
+    private double discount;
+    private double balance;
+    private UUID userId;
+    private final UUID id;
+
+    {
+        id = UUID.randomUUID();
+    }
+
+    public Amount(UUID userId) {
+        this.userId = userId;
+    }
 }
