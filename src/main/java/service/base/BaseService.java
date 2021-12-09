fd3978f34f4f7330b268b9169e78ac0b651d4ea1
muhammadsodiq <m.muhammadsodiq777@gmail.com>
2021-12-09 15:53:36 +0500

    heloo

--- src/main/java/service/base/BaseService.java
+++ src/main/java/service/base/BaseService.java
@@ -3,8 +3,8 @@ package service.base;
 import response.Response;
 
 public interface BaseService<T, S, L> extends Response {
-    String add(T t);
-    String remove(T t);
+    void add(T t);
+    void remove(T t);
     String edit(T t);
     T get(String str1, String str2);
     T getByIndex(int index);
