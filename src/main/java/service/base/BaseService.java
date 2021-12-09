62a858858c553c76641bb8725f28a789cd2e2a24
Muzaffar <bakhodirov.muzaffar2001@gmail.com>
2021-12-09 15:31:04 +0500

    History & UI

---
+++ src/main/java/service/base/BaseService.java
@@ -0,0 +1,16 @@
+package service.base;
+
+import response.Response;
+
+public interface BaseService<T, S, L> extends Response {
+    String add(T t);
+    String remove(T t);
+    String edit(T t);
+    T get(String str1, String str2);
+    T getByIndex(int index);
+    T get(S s);
+    L getList(S s);
+    L getList();
+    boolean check(String str1);
+    boolean isEmpty();
+}
