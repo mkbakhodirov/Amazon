720c0d38f1ee832fef7563783b2475429392218a
Muzaffar <bakhodirov.muzaffar2001@gmail.com>
2021-12-09 15:31:04 +0500

    History & UI

--- src/main/java/service/UsersService.java
+++ src/main/java/service/UsersService.java
@@ -1,5 +1,67 @@
 package service;
 
-public class UsersService {
+import model.user.User;
+import service.base.BaseService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersService implements BaseService<User, User, List<User>> {
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public String add(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public String remove(User user) {
+        user.setActive(false);
+    }
+
+    @Override
+    public String edit(User user) {
+        return null;
+    }
+
+    @Override
+    public User get(String str1, String str2) {
+        for (User user : users) {
+            if (user.isActive()) {
+                try {
+                    if (user.getPhoneNumber().equals(str1))
+                }
+            }
+        }
+    }
+
+    @Override
+    public User getByIndex(int index) {
+        return null;
+    }
+
+    @Override
+    public User get(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getList(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getList() {
+        return null;
+    }
+
+    @Override
+    public boolean check(String str1) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
