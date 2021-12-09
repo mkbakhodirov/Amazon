c1168aab3c8098e5568217f8476f5ff2d6f3b4d0
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2021-12-09 15:26:58 +0500

    updated

---
+++ src/main/java/uz/amazon/service/BaseService.java
@@ -0,0 +1,14 @@
+package uz.amazon.service;
+
+import uz.amazon.response.Response;
+
+    public interface BaseService<T, S, L> extends Response {
+        String add(T t);
+        T get(String str1, String str2);
+        T getByIndex(int index);
+        T get(S s);
+        boolean check(String str1);
+        boolean isEmpty();
+    }
+
+}
