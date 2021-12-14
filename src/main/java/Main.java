ad686b45ce8f4f3a32c5ab38677451fa7e0b060e
Muhammadaliy Furkatbek <muhammadaliy571632m@gmail.com>
2021-12-14 16:35:33 +0500

    Services

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,11 @@ import ui.HomeUI;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BaseUrl.urls.add("C:\\D\\PDP\\Java Backend FAANG\\Amazon\\src\\main\\java\\Database\\");
         BaseUrl.urls.add("C:\\Users\\Tohir\\Desktop\\Amazon\\src\\main\\java\\Database\\");
-        BaseUrl.urls.add("/home/muhammadsodiq/PDP Projects/Amazon1/src/main/java/Database\\");
-        BaseUrl.url = BaseUrl.urls.get(1);
         UsersService usersService = new UsersService();
         User superAdmin = new User(UserRole.SUPER_ADMIN, "qwe", "123");
+        usersService.add(superAdmin);
+        BaseUrl.url = BaseUrl.urls.get(0);
         usersService.add(superAdmin);
         HomeUI.home();
     }
--- src/main/java/bot/bot_replies/ReplyBot.java
+++ src/main/java/bot/bot_replies/ReplyBot.java
@@ -3,11 +3,6 @@ package bot.bot_replies;
 public interface ReplyBot {
 
      String START = "Assalamu alaykum \nBizning Online Amazon botimizga hush kelibsiz";
-     /// Registration
-     String SIGN_IN = "Sign in";
-     String SIGN_UP = "Sign up";
-     String SHARE_CONTACK = "Kontactimini ulashing";
-
      /// main manu
      String BUY = "\uD83D\uDCBC Sotib olish";
      String PAYMENT_TYPE = "\uD83D\uDCB8 To'lov turi";
@@ -15,7 +10,6 @@ public interface ReplyBot {
      String HISTORY = "\uD83D\uDC40 Histroy";
      String BACK = "\uD83D\uDD19 Back";
      String WEBPAGE = "\uD83D\uDD17 Our Website";
-     String PURCHASES = "\uD83D\uDED2 Basket";
 
           ///  buy menu
      String LAPTOPS = "\uD83D\uDCBB Laptops";
@@ -31,13 +25,11 @@ public interface ReplyBot {
      String MASTER_CARD = "\uD83D\uDCB5 Master Card";
      String VISA = "\uD83D\uDCB0 VISA";
 
-      //// inline keyboard markup words
-      String[] BUTTON_WORDS = {"-", "Clear", "+", "\uD83D\uDC4D", "Add to card","\uD83D\uDC4E and ❌"};
-     //
-     String MINUS = "-";
-     String CLEAR = "Clear";
-     String ADD = "+";
-     String ADD_TO_CARD = "Add to card";
-     String LIKE = "\uD83D\uDC4D";
-     String DISLIKE = "\uD83D\uDC4E and ❌";
+     String READY = "READY";
+     String CHOOSE = "CHOOSE";
+     String OPENING = "OPENING";
+     String PROCESSING = "PROCESSING";
+
+
+
 }
